7ae719a658d670781115898b8719716d9abe794f
Jmp13033 <jared7peck@gmail.com>
2026-09-17 15:41:58 -0400

    inventory manager

---
+++ dictionaries/inventory_manager.py
@@ -0,0 +1,22 @@
+# ============================================================
+#                  INVENTORY MANAGER
+# ============================================================
+
+# YOUR GOAL
+# ============================================================
+#
+# Build an Inventory Manager for a video game.
+#
+# The program should allow the player to:
+#
+# 1. View their inventory
+# 2. Add an item
+# 3. Remove an item
+# 4. Check if they have an item
+# 5. Use an item
+# 6. View how much gold they have
+# 7. Buy an item
+# 8. Search their inventory
+# 9. Show inventory statistics
+# 10. Quit the program
+#
